91fe7360898572ecadca87ffc6816a1e6f85a899
Luis Eduardo Rodríguez Ramírez <rodriguezramirezluiseduardo0@gmail.com>
2025-07-27 10:33:09 -0600

    Add more configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+console.log(process.env);
+
 export default defineConfig({
-  base: "two_years_with_vale",
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [react()],
 })
